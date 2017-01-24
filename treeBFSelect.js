f9bec2bc6d20b886597fd6dd184aecf1fef979aa
Timothy Yin <tyin92@gmail.com>
2017-01-24 16:19:47 -0500

    TreeBF instructions

--- treeBFSelect.js
+++ treeBFSelect.js
@@ -1,3 +1,25 @@
+// Implement a breadth-first method on a tree class.
+//
+// BFSelect accepts a filter function, calls that function on each of the nodes in Breadth-First order, and returns a flat array of node values of the tree for which the filter returns true.
+//
+// Example:
+//
+// var root1 = new Tree(1);
+// var branch2 = root1.addChild(2);
+// var branch3 = root1.addChild(3);
+// var leaf4 = branch2.addChild(4);
+// var leaf5 = branch2.addChild(5);
+// var leaf6 = branch3.addChild(6);
+// var leaf7 = branch3.addChild(7);
+//
+// root1.BFSelect(function (value, depth) {
+//   return value % 2;
+// }) //=> [1, 3, 5, 7]
+//
+// root1.BFSelect(function (value, depth) {
+//   return depth === 1;
+// }) //=> [2, 3]
+
 var Tree = function(value){
   this.value = value;
   this.children = [];
