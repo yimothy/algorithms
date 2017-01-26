7bd44560f5c5cea9edc9ed7fe02737affe8c5163
Timothy Yin <tyin92@gmail.com>
2017-01-25 20:10:01 -0500

    Added instructions

--- tree.js
+++ tree.js
@@ -1,3 +1,6 @@
+// Someone wrote a tree implementation, but they forgot to finish writing the definitions for addChild and contains.
+//
+// Help this person finish their tree by adding in the missing code for these methods.
 var treeMaker = function(value){
   var newTree = Object.create(treeMaker.methods);
   newTree.value = value;
