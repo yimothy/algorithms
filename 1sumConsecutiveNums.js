cb509c40e98f8480dc820bdbc6b7e407a99c867f
Timothy Yin <tyin92@gmail.com>
2017-01-31 14:57:22 -0500

    Completed 1consecutiveSum

--- 1sumConsecutiveNums.js
+++ 1sumConsecutiveNums.js
@@ -5,5 +5,20 @@
 //
 //
 // Return new array with all consecutive like values summed
-// 
+//
 // [1, 3, 4, 4, 9, 0] => [1, 3, 8, 9, 0]"""
+
+function consecutiveSum(array) {
+	let result = [];
+	let tempSum = 0;
+	for(let i = 0; i < array.length; i++) {
+		let curr = array[i];
+		let next = array[i+1];
+		tempSum += curr;
+		if(curr !== next) {
+			result.push(tempSum);
+			tempSum = 0;
+		}
+	}
+	return result;
+}
